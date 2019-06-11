c0abe9bc1a78e4db3eb964b11e394b4fd649c08c
Spencer LaPray <sblapray@yahoo.com>
2019-06-10 21:00:39 -0600

    successfully hitting the github api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { loading, users } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "GitHub Finder",
-    icon: "fab fa-github"
-  };
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "GitHub Finder",
+  icon: "fab fa-github"
+};
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
