459692d50afc5280a0f7732309b639ead00a13f7
Spencer LaPray <sblapray@yahoo.com>
2019-06-10 21:46:55 -0600

    search github and clear users from state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import "./App.css";
 
 class App extends Component {
@@ -10,17 +11,21 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  // Search GitHub Users
+  searchUsers = async text => {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${
+      `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+    console.log(res.data.items);
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-    this.setState({ users: res.data, loading: false });
-  }
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
     const { loading, users } = this.state;
@@ -28,6 +33,11 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={loading} users={users} />
         </div>
       </div>
