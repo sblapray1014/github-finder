cd6b3480aff53ea92f6cac3953afb55b71f7667e
Spencer LaPray <sblapray@yahoo.com>
2019-06-10 23:44:23 -0600

    implemented useEffect and useState hooks

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,120 +1,117 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired
-  };
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user;
 
-  render() {
-    const {
-      name,
-      company,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user;
-
-    const { loading, repos } = this.props;
-
-    if (loading) return <Spinner />;
-    return (
-      <Fragment>
-        <Link to="/" className="btn btn-light">
-          Back
-        </Link>
+  if (loading) return <Spinner />;
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-light">
+        Back
+      </Link>
+      <button className="btn btn-light">
         Hireable:{" "}
         {hireable ? (
           <i className="fas fa-check text-success" />
         ) : (
           <i className="fas fa-times-circle text-danger" />
         )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              alt=""
-              className="round-img"
-              style={{ width: "150px" }}
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              Visit GitHub Profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <Fragment>
-                    <strong>Username: </strong> {login}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <Fragment>
-                    <strong>Company: </strong> {company}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <Fragment>
-                    <strong>Website: </strong> <a href={blog}>{blog}</a>
-                  </Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
+      </button>
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt=""
+            className="round-img"
+            style={{ width: "150px" }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
         </div>
-        <div className="card text-center">
-          {followers > 0 ? (
-            <div className="badge badge-primary">Followers: {followers}</div>
-          ) : null}
-          {following > 0 ? (
-            <div className="badge badge-success">Following: {following}</div>
-          ) : null}
-          {public_repos > 0 ? (
-            <div className="badge badge-light">
-              Public Repos: {public_repos}
-            </div>
-          ) : null}
-          {public_gists > 0 ? (
-            <div className="badge badge-dark">Public Gists: {public_gists}</div>
-          ) : null}
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit GitHub Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong> <a href={blog}>{blog}</a>
+                </Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <div className="card text-center">
+        {followers > 0 ? (
+          <div className="badge badge-primary">Followers: {followers}</div>
+        ) : null}
+        {following > 0 ? (
+          <div className="badge badge-success">Following: {following}</div>
+        ) : null}
+        {public_repos > 0 ? (
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+        ) : null}
+        {public_gists > 0 ? (
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        ) : null}
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
+
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired
+};
 
 export default User;
